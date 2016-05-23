4f7f19590e16e7f7d63d004b59c31a17c5e1c420
HidekiAmeku <hidekiameku@gmail.com>
2016-05-23 12:48:55 +0900

    addJsonDecode

--- get_test.php
+++ get_test.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 $url = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20131024?';
- 
+
 $client = new GuzzleHttp\Client(['base_uri' => $url]);
 
 $response = $client->request('GET', '',['query'=>[
@@ -15,6 +15,12 @@ $response = $client->request('GET', '',['query'=>[
 												]
 									]);
 
-var_dump((string)$response->getBody());
+$arr = json_decode($response,true);
+var_dump($arr);
+exit;
+$result = $arr["roomBasicInfo"]["roomClass"];
+
+var_dump((string)$result->getBody());
 // jsonを配列に変換する
 // 空き部屋がある場合、値段だけを表示する
+
