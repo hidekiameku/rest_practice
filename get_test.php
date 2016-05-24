409ee72b82cc86951ece2671ef5824bb0eeffdd4
HidekiAmeku <hidekiameku@gmail.com>
2016-05-24 17:12:22 +0900

    addRoomnameAndChargeAndBlankroom

--- get_test.php
+++ get_test.php
@@ -1,31 +1,47 @@
 <?php
 require 'vendor/autoload.php';
+//use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
+
 $url = 'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20131024?';
 
 $client = new GuzzleHttp\Client(['base_uri' => $url]);
+try{
+	$response = $client->request('GET', '',['query'=>[
+													'applicationId' => '1074234954336267145', 
+													'format' => 'json',
+													'hotelNo' =>'5009',
+													'checkinDate' => '2016-05-28',
+													'checkoutDate' => '2016-06-29',
+													'adultNum' => '1',
+													'roomNum' => '1',
+													'sort' =>  'standard',
+													]
+										]);
+}catch(RequestException $e) {
+ 	echo "空室データはありません。";
+ 	return;
+}
+	$body = $response->getBody();
+	$stringBody = (string) $body;
+	$arr = json_decode($stringBody,true);
 
-$response = $client->request('GET', '',['query'=>[
-												'applicationId' => '1074234954336267145', 
-												'format' => 'json',
-												'largeClassCode' => 'japan',
-												'middleClassCode' => 'akita',
-												'smallClassCode' => 'tazawa',
-												'checkinDate' => '2016-08-01',
-												'checkoutDate' => '2016-08-03',
-												]
-									]);
+	$hotels = $arr['hotels'][0]['hotel'];
 
-$body = $response->getBody();
-//echo $body;
-$stringBpdy = (string) $body;
-$tenBytes = $body->read(10);
-//$remainingBytes = $body->getContens();
-$arr = json_decode($stringBpdy);
-var_dump($arr);
-exit;
-//$result = $arr["roomBasicInfo"]["roomClass"];
+	$hotel = $hotels[0]['hotelBasicInfo']['hotelName'];
 
-//var_dump($result->getBody());
-// jsonを配列に変換する
-// 空き部屋がある場合、値段だけを表示する
+	echo "ホテル名:". $hotel."\n";
 
+	foreach ($hotels as $key => $val) {
+		if($key === 0){
+			continue;
+		}
+
+	$roomName = $val['roomInfo'][0]['roomBasicInfo']['roomName'];
+	$charge = $val['roomInfo'][1]['dailyCharge']['rakutenCharge'];
+
+	echo "ルームネーム:".$roomName."\n";
+	echo "値段:".$charge."円\n";
+
+	}
